6b5bc1f14533234e0a90aa10f998fec88c3da1ec
Steve McCoy <s@mccoy.space>
2016-12-10 01:43:55 +0000

    Remove sun-clear.png and change show_image to take an argument.

--- show_image.py
+++ show_image.py
@@ -3,9 +3,13 @@
 # Be in these groups if you don't want to be root:
 # video input gpio i2c
 
+import sys
 from sense_hat import SenseHat
+
+if len(sys.argv) < 2:
+        sys.exit('I need the wunderground key pathname.')
 
 h = SenseHat()
 h.set_rotation(0)
 h.low_light = True
-h.load_image('sun-clear.png')
+h.load_image(sys.argv[1])
